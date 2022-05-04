33860cee376695c530a5ce90dc51523a1acb2f0a
TDCIAN <tdcian71@gmail.com>
2022-05-04 13:53:45 +0900

    make project

---
+++ RxWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxWeather
+//
+//  Created by JeongminKim on 2022/05/04.
+//
+// api.openweathermap.org/data/2.5/weather?q=Cheongju-si&appid=d97a141fbe206743133efac99624b767
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
